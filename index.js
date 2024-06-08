7f45221bab4f56745c34ed35265dd04b37ca5c43
Kwame Frimpong-Boateng <frimpong@Kwames-MacBook-Air.local>
2024-06-08 16:47:29 +0000

    add toggle functionality

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-window.alert("HELLO THERE JAVASCRIPT IN THE HOUSE Y'ALL !!")
+// select element
+let iconsContainer = document.querySelectorAll(".icons-container");
+console.log(iconsContainer);
+
+
+iconsContainer.forEach(iconContainer => {
+    iconContainer.addEventListener("click", () => {
+        console.log("click");
+
+
+        let openIcon = iconContainer.children[0];
+        openIcon.classList.toggle("toggle");
+
+        let questionItem= iconContainer.parentElement;
+        console.log(questionItem);
+
+        let answerContainer = questionItem.nextElementSibling;
+        console.log(answerContainer);
+        answerContainer.classList.toggle("toggle-answer");
+        
+    })
+})
